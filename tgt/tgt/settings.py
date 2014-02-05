4b780dc8190a39eee59844adc18d116e1db54f16
akhotan10 <akhotan10@gmail.com>
2014-02-04 18:56:38 -0800

    Update settings.py

--- tgt/tgt/settings.py
+++ tgt/tgt/settings.py
@@ -24,6 +24,13 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    '<workspace>/tango_with_django_project/templates/',
+)
+
 ALLOWED_HOSTS = []
 
 
