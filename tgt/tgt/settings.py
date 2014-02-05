8f25b2aadc48d1fe8a98ed03497e6073a4ea3cd3
akhotan10 <akhotan10@gmail.com>
2014-02-04 19:04:10 -0800

    Update settings.py

--- tgt/tgt/settings.py
+++ tgt/tgt/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
@@ -28,7 +29,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/Users/apk2/ThreeGoodThings/tgt/templates/',
+    'BASE_DIR',
 )
 
 ALLOWED_HOSTS = []
